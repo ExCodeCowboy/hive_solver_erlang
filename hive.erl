d37d4cfe23a467788d08f6ad8406c23c8062d08e
ExCowboyCoder <me@jeremykowen.com>
2014-03-16 15:49:54 -0500

    Hive almost done

--- hive.erl
+++ hive.erl
@@ -25,11 +25,16 @@ start_hive(EvalFunc,RanSolFunc,NeighborFunc) ->
 
 	HivePid = spawn(HiveStart),
 
-	lists:map(fun(A)->bee(HivePid,
-						 inactive,
-						 NeighborFunc,
-						 RanSolFunc,
-						 EvalFunc) end,lists:seq(1, 100)),
+	lists:map(fun(A)->
+		BeeSol = RanSolFunc(),
+		bee(HivePid,
+	     	BeeSol,
+	     	EvalFunc(BeeSol),
+		 	inactive,
+		 	NeighborFunc,
+		 	RanSolFunc,
+			EvalFunc,
+			BeeLifetime) end,lists:seq(1, 100)),
 	receive
 		{solution,Solution}->
 			io:format("Finishing Hive run.")
@@ -63,9 +68,9 @@ run_hive(EvalFunc,
 			{newBest, BeePid, BeeBest, BeeBestScore} ->
 				if BeeBestScore > BestSolutionScore ->
 					%Notify the inactive bees
-					lists:map(fun(I)->
-						I!{newBest,BeeBest,BeeBestScore}
-					end,InactiveBees),
+					lists:map(fun(I) ->
+						I ! {newBest,BeeBest,BeeBestScore}
+					end,queue:to_list(InactiveBees)),
 					ShortCall(BeeBest,
 							  BeeBestScore,
 							  InactiveBees,
@@ -76,24 +81,73 @@ run_hive(EvalFunc,
 							  InactiveBees,
 							  LivingBees)
 				end;
-
+			{comingHome, BeePid} ->
+				case queue:out(InactiveBees) of
+					{{_,LeavingBeePid},RemainingInactive} ->
+						LeavingBeePid ! {forage},
+						ShortCall(BestSolution,
+								 BestSolutionScore,
+								 queue:in(BeePid,RemainingInactive),
+								 TotalBees);						
+					{empty,RemainingInactive} ->
+						ShortCall(BestSolution,
+								 BestSolutionScore,
+								 queue:in(BeePid,RemainingInactive),
+								 TotalBees)
+				end;
 			{dead,BeePid} ->
 				RemainingBees = LivingBees-1,
-				io:format(" ~p pings~n",[RemainingBees]),
+				io:format("Bee ~p dies, ~p remaining ~n",[BeePid,RemainingBees]),
 				if RemainingBees > 0 ->
+						RemainingInactive = 
+							queue:filter(fun(I)-> I/=BeePid end,InactiveBees),
 						ShortCall(BestSolution,
 								  BestSolutionScore,
-								 InactiveBees,
-								 RemainingBees);
+								  RemainingInactive,
+								  RemainingBees);
 				true ->
 					CallBack(BestSolution)
 				end
 		end.
 
 bee(HivePid,
+	BestSolution,
+	BestSolutionScore,
 	State,
 	NeighborFunc,
 	RanSolFunc,
-	EvalFunc) ->
+	EvalFunc,
+	LifeTime) ->
+		ShortCall = fun(BestSolution_S,
+						BestSolutionScore_S,
+						State_S) ->
+			bee(HivePid,
+				BestSolution_S,
+				BestSolutionScore_S,
+				State_S,
+				NeighborFunc,
+				RanSolFunc,
+				EvalFunc,
+				LifeTime-1)
+		end,
 
-	HivePid ! {dead,self()}.
+	if LifeTime == 0 ->
+		HivePid ! {dead,self()};
+	true ->
+		ShortCall(BestSolution,
+			      BestSolutionScore,
+			      State)
+	end.
+
+
+
+
+
+
+
+
+
+
+
+
+
