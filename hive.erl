9f4e685b1cd921b09e407d49167562d4f5f7a15c
ExCowboyCoder <me@jeremykowen.com>
2014-03-19 23:19:25 -0500

    Split out bees and restructured with records

--- hive.erl
+++ hive.erl
@@ -1,7 +1,7 @@
 -module(hive).
 -export([start_hive/1]).
+-import(bee,[start_bee/4]).
 
--record(probDef, {evalFunc,ranSolFunc,neighborFunc}).
 -record(hiveState,{bestSolution=0,
 				   bestSolutionScore=0,
 				   inactiveBees=queue:new(),
@@ -11,9 +11,7 @@
 -define(NumActive, 50).
 -define(NumScouts, 30).
 -define(NumInactive,15).
--define(ActiveTime, 100).
--define(Accuracy,95).
--define(WagglePersuation,70).
+
 
 
 start_hive(Prob) ->
@@ -39,20 +37,11 @@ start_hive(Prob) ->
 				A =< ?NumInactive+?NumScouts ->
 					scouting;
 				true->
-					{active,?ActiveTime}
+					active
 				end,
-			BeeSol = (Prob#probDef.ranSolFunc)(),
 			spawn(
 				fun()->
-					random:seed(crypto:rand_uniform(1, 200000000),
-						crypto:rand_uniform(1, 200000000),
-						crypto:rand_uniform(1, 200000000)),
-					bee(HivePid,
-			     	BeeSol,
-			     	(Prob#probDef.evalFunc)(BeeSol),
-				 	Status,
-				 	Prob,
-					?BeeLifetime)
+					start_bee(HivePid,Prob,Status,?BeeLifetime)
 				end) 
 		end,lists:seq(1, TotalBees)),
 	receive
@@ -65,7 +54,7 @@ run_hive(State,
 		 CallBack)->
 
 		receive 
-			{newBest, BeePid, BeeBest, BeeBestScore} ->
+			{newBest, _BeePid, BeeBest, BeeBestScore} ->
 				%io:format("Bee ~p reports ~p as new personal best of ~p vs ~p ~n",
 				%	[BeePid,BeeBest, BeeBestScore,BestSolutionScore]),
 					lists:map(fun(I) ->
@@ -137,116 +126,7 @@ run_hive(State,
 				end
 		end.
 
-bee(HivePid,
-	BestSolution,
-	BestSolutionScore,
-	State,
-	Prob,
-	LifeTime) ->
-		ShortCall = fun(BestSolution_S,
-						BestSolutionScore_S,
-						State_S) ->
-			bee(HivePid,
-				BestSolution_S,
-				BestSolutionScore_S,
-				State_S,
-				Prob,
-				LifeTime-1)
-		end,
-	
-	BeePid = self(),
-	
-	if LifeTime == 0 ->
-		HivePid ! {dead,BeePid};
-	true ->
-		case State of
-			initializeInactive->
-				%signal the hive you are coming home
-				HivePid!{startLanded,BeePid},
-				%then go inactive
-				ShortCall(
-					BestSolution,
-					BestSolutionScore,
-					inactive);
-			{active,RemainingIterations}->
-				if RemainingIterations == 0 ->
-					%then go inactive
-					HivePid!{comingHome,BeePid},
-					ShortCall(
-						BestSolution,
-						BestSolutionScore,
-						inactive);
-				true->
-					%try neighboor
-					Neighbor = (Prob#probDef.neighborFunc)(BestSolution),
-					NeighborScore = (Prob#probDef.evalFunc)(Neighbor),
-					%if neghbor is better
-					BetterChoice = fuzzy_choice(NeighborScore>BestSolutionScore),
-					if BetterChoice ->
-						%need to insert possible mistakes that bee makes
-						HivePid!{newBest, BeePid, Neighbor, NeighborScore},
-						ShortCall(Neighbor,
-								  NeighborScore,
-								  {active,RemainingIterations});
-					true->
-						ShortCall(BestSolution,
-								  BestSolutionScore,
-								  {active,RemainingIterations-1})
-					end
-				end;
-			inactive->
-				receive
-					{forage}->
-						ShortCall(BestSolution,
-							BestSolutionScore,
-							{active,?ActiveTime});
-					{newBest,BeeBest,BeeBestScore}->
-						Persuaded = ?WagglePersuation =< random:uniform(100),
-						BetterChoice = BeeBestScore>BestSolutionScore,
-						if BetterChoice and Persuaded ->
-							io:format("~p learned new best ~p ~n",[BeePid,BeeBest]),
-							ShortCall(BeeBest,
-								BeeBestScore,
-								inactive);
-						true->
-							ShortCall(BestSolution,
-							BestSolutionScore,
-							inactive)
-					end
-				after 50->
-					ShortCall(BestSolution,
-							BestSolutionScore,
-							inactive)
-				end;
-			scouting->
-				%try neighboor
-				RanSol = (Prob#probDef.ranSolFunc)(),
-				RanSolScore = (Prob#probDef.evalFunc)(RanSol),
-				%if neghbor is better
-				BetterChoice = fuzzy_choice(RanSolScore>BestSolutionScore),
-				if BetterChoice ->
-					%need to insert possible mistakes that bee makes
-					HivePid!{newBest, BeePid, RanSol, RanSolScore},
-					ShortCall(RanSol,
-							  RanSolScore,
-							  scouting);
-				true->
-					ShortCall(BestSolution,
-							  BestSolutionScore,
-							  scouting)
-				end
-		end
-	end.
 
-
-fuzzy_choice(Truth)->
-	Chance = random:uniform(100),
-	if Chance > ?Accuracy -> 
-		%io:format("Bee ~p made a mistake. ~n",[self()]),
-		not Truth;
-	true->
-		Truth
-	end.
 
 
 
